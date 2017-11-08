7debaf7222f52037ec5b119cf0e7461726e162eb
GOKHANAGA <gokhanirmak2@hotmail.com>
2017-11-08 18:31:48 +0300

    Add files via upload

---
+++ 20171108MDI/AddingCommonMethodToForms/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddingCommonMethodToForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resimFormuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ImageForm ımg = new ImageForm();
+            ımg.MdiParent = this;
+            ımg.Show();
+        }
+
+        private void metinFormuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TextForm txtForm = new TextForm();
+            txtForm.MdiParent = this;
+            txtForm.Show();
+        }
+
+        private void kapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ((Child1)this.ActiveMdiChild).CloseForm();
+            }
+            catch
+            {
+                MessageBox.Show("Aktif Form Bulunamadı.");
+            }
+        }
+    }
+}
---
+++ 20171108MDI/AddingCommonMethodToForms/ImageForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddingCommonMethodToForms
+{
+    public partial class ImageForm : Child1 /*----Inheritance----*/
+    {
+        public ImageForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
